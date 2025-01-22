2f33afe4fa7a5f2505b436c7bd66c626254c3a4f
Carl Barrdahl <carlbarrdahl@gmail.com>
2025-01-23 00:01:53 +0800

    Retry wait for event

--- packages/allo-app/hooks/use-wait-for-event.ts
+++ packages/allo-app/hooks/use-wait-for-event.ts
@@ -3,13 +3,18 @@
 import { useWalletClient } from "wagmi";
 import { Abi, type Hex, parseEventLogs } from "viem";
 import { getTransactionReceipt } from "viem/actions";
+import pRetry, { AbortError } from "p-retry";
 
 export function useWaitForEvent(abi: Abi) {
   const { data: client } = useWalletClient();
   return <T>(hash: Hex, eventName: string): Promise<T> => {
     if (!client) throw new Error("WalletClient not found");
-    return getTransactionReceipt(client, { hash })
-      .then(({ logs }) => parseEventLogs({ abi, logs, eventName }))
-      .then((events) => events?.[0]?.args as T);
+    return pRetry(
+      () =>
+        getTransactionReceipt(client, { hash })
+          .then(({ logs }) => parseEventLogs({ abi, logs, eventName }))
+          .then((events) => events?.[0]?.args as T),
+      { retries: 5 }
+    );
   };
 }
