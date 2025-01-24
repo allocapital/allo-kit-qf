4ba76d13f45c2eb0f0e86f82518293854fe84ba7
Carl Barrdahl <carlbarrdahl@gmail.com>
2025-01-24 20:21:32 +0100

    Add fetchTokenPrice and index amountInUSD

--- packages/allo-indexer/src/index.ts
+++ packages/allo-indexer/src/index.ts
@@ -2,7 +2,14 @@ import { Context, Event, ponder } from "ponder:registry";
 import schemas from "ponder:schema";
 import { Address, erc20Abi, Hex } from "viem";
 import pRetry, { AbortError } from "p-retry";
+import NodeFetchCache, { FileSystemCache } from "node-fetch-cache";
 
+const fetch = NodeFetchCache.create({
+  shouldCacheResponse: (res) => res.ok,
+  cache: new FileSystemCache({ ttl: 1000 * 60 * 60, cacheDirectory: ".cache" }),
+});
+
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
 const PINATA_GATEWAY_URL = process.env.PINATA_GATEWAY_URL;
 const PINATA_GATEWAY_KEY = process.env.PINATA_GATEWAY_KEY;
 
@@ -59,12 +66,16 @@ ponder.on("Allocator:Allocate", async ({ event, context }) => {
 
   const [decimals, symbol] = await fetchToken(token, context.client);
 
+  const tokenPrice = await fetchTokenPrice(symbol);
+  const amountInUSD = BigInt(Number(amount) * tokenPrice);
+
   await context.db.insert(schemas.allocation).values({
     id: `${event.log.id}`,
     strategy: event.log.address,
     to,
     from,
-    amount: amount.toString(),
+    amount,
+    amountInUSD,
     token: { address: token, decimals, symbol },
     tokenAddress: token,
     createdAt: toSeconds(Date.now()),
@@ -76,30 +87,29 @@ const registrationId = (event: Event) =>
   `${event.args.project}_${event.log.address}_${event.args.index}` as Hex;
 
 async function fetchMetadata(cid: string) {
+  console.log("Fetching metadata for:", cid);
   return pRetry(
     () => {
       const ipfsUrl = `https://${PINATA_GATEWAY_URL}/ipfs/${cid}?pinataGatewayToken=${PINATA_GATEWAY_KEY}`;
       return cid
-        ? fetch(ipfsUrl)
-            .then(async (r) => {
-              if (r.status === 404) {
-                throw new AbortError(r.statusText);
-              }
-              return (await r.json()) as {
-                title: string;
-                description?: string;
-                image?: string;
-              };
-            })
-            .catch((err) => ({}))
+        ? fetch(ipfsUrl).then((r) => {
+            if (!r.ok) throw new Error(r.statusText);
+            if (r.status === 404) {
+              throw new AbortError(r.statusText);
+            }
+            return r.json();
+          })
         : {};
     },
     { retries: MAX_RETRY_COUNT }
-  );
+  ).catch((err) => {
+    console.log("fetchMetadata error:", err);
+    return {};
+  });
 }
 
 async function fetchToken(address: Address, client: Context["client"]) {
-  // TODO: Add cache
+  console.log("Fetching token decimal and symbol for: ", address);
   return pRetry(
     () => {
       const tokenContract = {
@@ -114,6 +124,32 @@ async function fetchToken(address: Address, client: Context["client"]) {
     },
     { retries: MAX_RETRY_COUNT }
   );
+}
+
+async function fetchTokenPrice(symbol: string) {
+  console.log("Fetching token price for symbol:", symbol);
+  console.log({ ALCHEMY_API_KEY, symbol });
+  if (!ALCHEMY_API_KEY) return 0;
+  return pRetry(
+    () => {
+      return fetch(
+        `https://api.g.alchemy.com/prices/v1/${ALCHEMY_API_KEY}/tokens/by-symbol?symbols=${symbol.toLowerCase()}`,
+        { headers: { accept: "application/json" } }
+      )
+        .then(async (r) => {
+          if (!r.ok) throw new Error(r.statusText);
+
+          return (await r.json()) as {
+            data: { prices: { value: string }[] }[];
+          };
+        })
+        .then((r) => Number(r.data?.[0]?.prices[0]?.value ?? 0));
+    },
+    { retries: MAX_RETRY_COUNT }
+  ).catch((err) => {
+    console.log("fetchTokenPrice error:", err);
+    return 0;
+  });
 }
 
 function toSeconds(ms: number) {
