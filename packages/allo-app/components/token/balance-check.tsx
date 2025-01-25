63fef7e451c40aaed882eb060479ad87d536decd
Carl Barrdahl <carlbarrdahl@gmail.com>
2025-01-25 07:11:29 +0100

    Move BalanceCheck to components/token

--- packages/allo-app/components/token/balance-check.tsx
+++ packages/allo-app/components/token/balance-check.tsx
@@ -1,16 +1,19 @@
 import { PropsWithChildren } from "react";
 import { useAccount, useBalance } from "wagmi";
-import { Button } from "./ui/button";
+import { Button } from "../ui/button";
 
-export function BalanceCheck({ children }: PropsWithChildren) {
+export function BalanceCheck({
+  children,
+  amount = 0n,
+}: PropsWithChildren<{ amount?: bigint }>) {
   const { address } = useAccount();
-  const { data: { value = 0 } = {}, isPending } = useBalance({
+  const { data: { value = 0n } = {}, isPending } = useBalance({
     address,
   });
 
   if (isPending) return <Button variant={"outline"} isLoading />;
 
-  if (value > 0) return <>{children}</>;
+  if (value > amount) return <>{children}</>;
 
   return (
     <Button disabled variant="ghost">
