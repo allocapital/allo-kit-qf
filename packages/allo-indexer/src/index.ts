23525c6011b32ba622ebb6a734ca0aab3d959cef
Carl Barrdahl <carlbarrdahl@gmail.com>
2025-02-03 15:37:34 +0100

    Improve indexer

--- packages/allo-indexer/src/index.ts
+++ packages/allo-indexer/src/index.ts
@@ -1,13 +1,8 @@
 import { Context, Event, ponder } from "ponder:registry";
 import schemas from "ponder:schema";
 import { Address, erc20Abi, Hex, zeroAddress } from "viem";
-import pRetry, { AbortError } from "p-retry";
-import NodeFetchCache, { FileSystemCache } from "node-fetch-cache";
-
-const fetch = NodeFetchCache.create({
-  shouldCacheResponse: (res) => res.ok,
-  cache: new FileSystemCache({ ttl: 1000 * 60 * 60, cacheDirectory: ".cache" }),
-});
+import pRetry from "p-retry";
+import { cachedFetchWithRetry } from "./lib/fetch";
 
 const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
 const PINATA_GATEWAY_URL = process.env.PINATA_GATEWAY_URL;
@@ -23,7 +18,7 @@ ponder.on("Strategy:Initialize", async ({ event, context }) => {
     .values({
       address: event.log.address,
       name: strategyName,
-      createdAt: toSeconds(Date.now()),
+      createdAt: Number(event.block.timestamp),
     })
     .onConflictDoNothing();
 });
@@ -43,8 +38,8 @@ ponder.on("Registry:Register", async ({ event, context }) => {
       metadata,
       isApproved: false,
       data,
-      createdAt: toSeconds(Date.now()),
-      updatedAt: toSeconds(Date.now()),
+      createdAt: Number(event.block.timestamp),
+      updatedAt: Number(event.block.timestamp),
     })
     .onConflictDoNothing();
 });
@@ -56,7 +51,7 @@ ponder.on("Registry:Approve", async ({ event, context }) => {
     .update(schemas.registration, { id: registrationId(event) })
     .set(() => ({
       isApproved: true,
-      updatedAt: toSeconds(Date.now()),
+      updatedAt: Number(event.block.timestamp),
       review,
     }));
 });
@@ -78,7 +73,7 @@ ponder.on("Allocator:Allocate", async ({ event, context }) => {
     amountInUSD,
     token: { address: token, decimals, symbol },
     tokenAddress: token,
-    createdAt: toSeconds(Date.now()),
+    createdAt: Number(event.block.timestamp),
   });
 });
 
@@ -88,24 +83,14 @@ const registrationId = (event: Event) =>
 
 async function fetchMetadata(cid: string) {
   console.log("Fetching metadata for:", cid);
-  return pRetry(
-    () => {
-      const ipfsUrl = `https://${PINATA_GATEWAY_URL}/ipfs/${cid}?pinataGatewayToken=${PINATA_GATEWAY_KEY}`;
-      return cid
-        ? fetch(ipfsUrl).then((r) => {
-            if (!r.ok) throw new Error(r.statusText);
-            if (r.status === 404) {
-              throw new AbortError(r.statusText);
-            }
-            return r.json();
-          })
-        : {};
-    },
-    { retries: MAX_RETRY_COUNT }
-  ).catch((err) => {
-    console.log("fetchMetadata error:", err);
-    return {};
-  });
+  return cid
+    ? cachedFetchWithRetry(
+        `https://${PINATA_GATEWAY_URL}/ipfs/${cid}?pinataGatewayToken=${PINATA_GATEWAY_KEY}`
+      ).catch((err) => {
+        console.log("fetchMetadata error:", err);
+        return {};
+      })
+    : {};
 }
 
 async function fetchToken(address: Address, client: Context["client"]) {
@@ -128,7 +113,7 @@ async function fetchToken(address: Address, client: Context["client"]) {
   );
 }
 
-async function fetchTokenPrice(symbol: string) {
+async function fetchTokenPrice(symbol: string): Promise<string> {
   console.log("Fetching token price for symbol:", symbol);
   if (!ALCHEMY_API_KEY) {
     console.log("ALCHEMY_API_KEY not set. Cannot fetch token price.");
@@ -136,28 +121,18 @@ async function fetchTokenPrice(symbol: string) {
   }
   // Our ERC20Mock token used for testing
   if (symbol === "tUSD") symbol = "USDC";
-  console.log({ ALCHEMY_API_KEY, symbol });
-  if (!ALCHEMY_API_KEY) return 0;
-  return pRetry(
-    () => {
-      return fetch(
-        `https://api.g.alchemy.com/prices/v1/${ALCHEMY_API_KEY}/tokens/by-symbol?symbols=${symbol.toLowerCase()}`,
-        { headers: { accept: "application/json" } }
-      )
-        .then(async (r) => {
-          if (!r.ok) throw new Error(r.statusText);
-
-          return (await r.json()) as {
-            data: { prices: { value: string }[] }[];
-          };
-        })
-        .then((r) => String(r.data?.[0]?.prices[0]?.value ?? 0));
-    },
-    { retries: MAX_RETRY_COUNT }
-  ).catch((err) => {
-    console.log("fetchTokenPrice error:", err);
-    return "0";
-  });
+  if (!ALCHEMY_API_KEY) return "0";
+  return cachedFetchWithRetry<{
+    data: { prices: { value: string }[] }[];
+  }>(
+    `https://api.g.alchemy.com/prices/v1/${ALCHEMY_API_KEY}/tokens/by-symbol?symbols=${symbol.toLowerCase()}`,
+    { headers: { accept: "application/json" } }
+  )
+    .then((r) => String(r.data?.[0]?.prices[0]?.value ?? "0"))
+    .catch((err) => {
+      console.log("fetchTokenPrice error:", err);
+      return "";
+    });
 }
 
 function toSeconds(ms: number) {
