c688cbbb03b13ad5c668b8fbd50eae03f68e4316
Carl Barrdahl <carlbarrdahl@gmail.com>
2025-02-11 14:48:06 +0100

    Update DirectGrants contract

--- packages/hardhat/contracts/strategies/DirectGrants.sol
+++ packages/hardhat/contracts/strategies/DirectGrants.sol
@@ -2,20 +2,42 @@
 pragma solidity >=0.8.0 <0.9.0;
 
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
-
+import { Strategy } from "../base/Strategy.sol";
 import { Allocator } from "../extensions/Allocator.sol";
 import { Registry, IRegistry } from "../extensions/Registry.sol";
-import { Strategy } from "../base/Strategy.sol";
 
 /**
- * A simple DirectGrants contract that allows ERC20 transfers to multiple recipients
+ * A simple DirectGrants contract that allows ERC20 transfers to multiple approved projects
  * @author AlloCapital
  */
+
 contract DirectGrants is Strategy, Allocator, Registry, Ownable {
     constructor(address owner) Ownable(owner) Strategy("DirectGrants") {}
 
     function register(address project, string memory metadataURI, bytes memory data) public override {
-        _register(project, metadataURI, data);
-        _approve(project, 0, "", ""); // Auto-approve projects
+        super.register(project, metadataURI, data);
+    }
+
+    function approve(
+        address project,
+        uint256 index,
+        string memory metadataURI,
+        bytes memory data
+    ) public override onlyOwner {
+        super.approve(project, index, metadataURI, data);
+    }
+
+    function allocate(
+        address[] memory recipients,
+        uint256[] memory amounts,
+        address token,
+        bytes[] memory data
+    ) public override onlyOwner {
+        super.allocate(recipients, amounts, token, data);
+    }
+
+    function _allocate(address to, uint256 amount, address token, bytes memory data) internal override {
+        require(projects[to][0].status == IRegistry.Status.approved, "Project not approved");
+        super._allocate(to, amount, token, data);
     }
 }
