93f302cdfc753f1e1c4529a3ddf523b44342b150
Carl Barrdahl <carlbarrdahl@gmail.com>
2025-02-13 13:35:42 +0100

    QuadraticFunding tutorial in progress

--- packages/hardhat/contracts/strategies/QuadraticFunding.sol
+++ packages/hardhat/contracts/strategies/QuadraticFunding.sol
@@ -8,10 +8,10 @@ import { Registry, IRegistry } from "../extensions/Registry.sol";
 import { Strategy } from "../base/Strategy.sol";
 
 /**
- * A simple QuadraticFunding contract that allows matching funds to be added to the Strategy and distributed
+ * A simple QuadraticFunding contract that allows matching funds to be added to the Strategy and distributed to projects by the owner
  * @author AlloCapital
  */
-contract QuadraticFunding is Strategy, Allocator, Ownable {
+contract QuadraticFunding is Strategy, Allocator, Registry, Ownable {
     address public immutable donationToken;
     address public immutable matchingToken;
 
@@ -24,9 +24,18 @@ contract QuadraticFunding is Strategy, Allocator, Ownable {
         matchingToken = _matchingToken;
     }
 
+    function approve(
+        address project,
+        uint256 index,
+        string memory metadataURI,
+        bytes memory data
+    ) public override onlyOwner {
+        super.approve(project, index, metadataURI, data);
+    }
+
     // Override allocate function to check if tokens are either donation or matching based on the recipient
     function _allocate(address to, uint256 amount, address token, bytes memory data) internal override nonReentrant {
-        if (to == address(this)) require(token == matchingToken, "Matching funds must be matching token");
+        if (to == address(this)) require(token == matchingToken, "Matching funds to strategy must be matching token");
         if (to != address(this)) require(token == donationToken, "Allocations to projects must be donation token");
 
         super._allocate(to, amount, token, data);
